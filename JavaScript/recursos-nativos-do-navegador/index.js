bd590597974168ac6f8489bf77856670b0b96f0d
Narcelio-Rodrigues <rodriguesnarcelio5@gmail.com>
2024-08-29 13:09:34 -0300

    mudanças na pasta

---
+++ JavaScript/recursos-nativos-do-navegador/index.js
@@ -0,0 +1,12 @@
+let nome = 'Narcelio'
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+// window.alert(nome)
+// window.prompt('Insira o seu nome:')
+
+/*nome = window.prompt('Insira o seu nome:')
+console.log(nome)*/
+

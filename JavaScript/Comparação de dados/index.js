79758bd068d9810fd6671321352a4a93bbecf56f
Narcelio-Rodrigues <rodriguesnarcelio5@gmail.com>
2024-08-30 13:19:10 -0300

    add resumo

--- JavaScript/Comparação de dados/index.js
+++ JavaScript/Comparação de dados/index.js
@@ -39,10 +39,48 @@ OPERADORES
 
 /*
 3. COMPARAÇÃO DE MAIOR OU MENOR
-*/
+    #Maior que (>): Verifica se o valor da esquerda é maior que o da direita.
+    */
+
+    console.log(10 > 5); // true
+    console.log('b' > 'a'); // true (comparação lexicográfica)
+
+    /*
+    #Maior ou igual (>=): Verifica se o valor da esquerda é maior ou igual ao da direita.
+    */
+    
+    console.log(10 >= 10); // true
+    console.log('a' >= 'a'); // true
+
+    /*
+    #Menor que (<): Verifica se o valor da esquerda é menor que o da direita.
+    */
+
+    console.log(5 < 10); // true
+    console.log('a' < 'b'); // true (comparação lexicográfica)
+
+    /*
+    #Menor ou igual (<=): Verifica se o valor da esquerda é menor ou igual ao da direita.
+    */
+
+    console.log(10 <= 10); // true
+    console.log('a' <= 'a'); // true
 
 /*
 4. COMPARAÇÃO DE TIPOS
-*/
+    #'typeof': Retorna o tipo de um valor, útil para verificar o tipo antes de comparar.
+    */
 
-console.log(5 == 5)
+    console.log(typeof 42); // "number"
+    console.log(typeof 'Hello'); // "string"
+    console.log(typeof true); // "boolean"
+
+    /*
+    #'instanceof': Verifica se um objeto %C3%A9 uma inst%C3%A2ncia de um determinado construtor.
+    */
+
+    let arr = [];
+    console.log(arr instanceof Array); // true
+    console.log(arr instanceof Object); // true (arrays são objetos)
+
+

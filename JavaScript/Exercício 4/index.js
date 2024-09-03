18e431629d5d3e468e236c0698312efac590bdca
Narcelio-Rodrigues <rodriguesnarcelio5@gmail.com>
2024-09-03 13:20:46 -0300

    resolvendo exercicio

---
+++ JavaScript/Exercício 4/index.js
@@ -0,0 +1,8 @@
+// Personegem 1
+const nomeDoPoder = prompt ('Informe o nome do poder do personagem:')
+
+const poderDeAtaque = parseFloat(prompt('Qual é o poder de ataque do ' + nomeDoPersonagem1 + '?'))
+
+const nomeDoPersonagem1 = prompt ('Informe o nome do personagem:')
+
+const PontosDeVida = parseFloat(prompt('quantos pontos de vida o' + nomeDoPersonagem1 + 'tem?')) 

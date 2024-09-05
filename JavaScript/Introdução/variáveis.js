66d741039b1b1895d167ae568c2487c09c7dfb0e
Narcelio-Rodrigues <rodriguesnarcelio5@gmail.com>
2024-09-05 12:41:41 -0300

    revisão sobre váriaveis

--- JavaScript/Introdução/variáveis.js
+++ JavaScript/Introdução/variáveis.js
@@ -1,3 +1,38 @@
+// Utilizar sempre o Camel case no JavaScript!
+// JavaScript possui tipagem dinâmica: infere os tipos de daos
+//JavaScript possui uma tipagem fraca
+
+var nomeDoAluno = 'Narcélio'; //string (texto: Utilize sempre aspas)
+var idade = 23; // number
+var altura = 1.77; // number.
+var estudando = true; // boolean (booleano: verdadeiro ou falso)
+
+//  PARA IMPRIMIR:
+console.log(nomeDoAluno)
+console.log(idade)
+console.log(altura)
+console.log(estudando)
+
+// PARA MOSTRAR O TIPO:
+console.log(typeof nomeDoAluno);
+
+var semConteudo; // declarando uma variável
+console.log(semConteudo)
+
+// undefined: variável que foi declarada mais não recebeu nenhum conteúdo.
+
+// !Não utilize o var nos códigos JavaScript
+
+let notaDoAluno = 10;
+const mediaDoAluno = 8;
+
+notaDoAluno = 9;
+mediaDoAluno = 5; // Não é permitido!
+
+console.log(notaDoAluno);
+console.log(mediaDoAluno);
+
+
 //VARIÁVEIS
 //Servem para armazenar os dados para podermos usá-los no código
 

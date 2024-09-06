fc6201c03f60cd3fa64152d8e3c2863b88eb5268
Narcelio-Rodrigues <rodriguesnarcelio5@gmail.com>
2024-09-06 12:56:27 -0300

    revisão 2

--- JavaScript/Introdução/tempCodeRunnerFile.js
+++ JavaScript/Introdução/tempCodeRunnerFile.js
@@ -1,8 +1,4 @@
-const nota1 = 10;
-const nota2 = 8;
+const numero = 16;
+const raizDoNumero = Math.sqrt(numero);
 
-const somaDasNotas = nota1 + nota2;
-let mediaDasNotas = somaDasNotas / 2;
-
-console.log('Soma das notas =', somaDasNotas);
-console.log('Média das notas = ', mediaDasNotas);
+console.log('Raiz quadrada de 16 =', raizDoNumero);

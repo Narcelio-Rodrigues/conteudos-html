076856be569d9a69af3b4c151fe0d923ef39b293
Narcelio-Rodrigues <rodriguesnarcelio5@gmail.com>
2024-09-19 13:08:19 -0300

    Exercicio de condições

---
+++ JavaScript/JavaScript - Curso Em Vídeo/Aula 11 – Condições (Parte 1)/index.js
@@ -0,0 +1,8 @@
+var velocidade = 61
+console.log(`A velocidade do seu carro é ${velocidade} km/h`)
+
+if (velocidade > 60) { //Condição Simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)

06f8deca330e401501d3d5e3a8b2f939b80d3132
Narcelio-Rodrigues <rodriguesnarcelio5@gmail.com>
2024-10-03 13:49:53 -0300

    finalizando o exercício 1

--- JavaScript/JavaScript - Curso Em Vídeo/Exercícios JavaScript/script.js
+++ JavaScript/JavaScript - Curso Em Vídeo/Exercícios JavaScript/script.js
@@ -2,14 +2,16 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    // var hora = data.getHours()
-    var hora = 1
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         img.src = '/JavaScript/JavaScript - Curso Em Vídeo/Exercícios JavaScript/img/fotomanha.png'
+        document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora < 18) {
         img.src = '/JavaScript/JavaScript - Curso Em Vídeo/Exercícios JavaScript/img/fototarde.png'
+        document.body.style.background = '#b9846f'
     } else{
         img.src = '/JavaScript/JavaScript - Curso Em Vídeo/Exercícios JavaScript/img/fotonoite.png'
+        document.body.style.background = '#515154'
     }
 }

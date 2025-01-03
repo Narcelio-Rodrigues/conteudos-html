9ee91ed8a5864f1967127dfaa50e20b93cc05869
Narcelio-Rodrigues <rodriguesnarcelio5@gmail.com>
2025-01-03 00:27:46 -0300

    COMENTÁRIO

--- JavaScript/JavaScript - OBT/Operadores e expressões/index.js
+++ JavaScript/JavaScript - OBT/Operadores e expressões/index.js
@@ -7,9 +7,26 @@
 // let b = 1 + "1"
 // console.log(b)
 
-let d = 34
-let v = 63
+// let d = 34
+// let v = 63
 
-v = '63'
+// v = '63'
 
-console.log(d + v)
+// console.log(d + v)
+
+/*
+COMENTÁRIO DE VÁRIAS LINHAS
+COMENTÁRIO DE VÁRIAS LINHAS
+COMENTÁRIO DE VÁRIAS LINHAS
+COMENTÁRIO DE VÁRIAS LINHAS
+COMENTÁRIO DE VÁRIAS LINHAS
+COMENTÁRIO DE VÁRIAS LINHAS
+*/
+
+// COMENTÁRIO DE UMA LINHA
+
+// let a = 'Javascript'.length + 2 * 8
+// console.log(a)
+
+let x = ((3 + "javascript".length) * "HTML"[3].length + "CSS"[0]) + 10 % 3
+console.log(x)

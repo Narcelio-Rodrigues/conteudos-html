cf14cc155c62fac1b2b70433f72ebd90c0bb87d2
Narcelio-Rodrigues <rodriguesnarcelio5@gmail.com>
2025-01-03 00:06:09 -0300

    Operadores e expressões

--- JavaScript/JavaScript - OBT/Operadores e expressões/index.js
+++ JavaScript/JavaScript - OBT/Operadores e expressões/index.js
@@ -1,5 +1,15 @@
 // let a = 5
 // console.log(a)
 
-let nome = 'camelo ' + 'Pontes'
-console.log(nome)
+// let nome = 'Camelo' + ' Pontes'
+// console.log(nome)
+
+// let b = 1 + "1"
+// console.log(b)
+
+let d = 34
+let v = 63
+
+v = '63'
+
+console.log(d + v)

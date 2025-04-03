d40d82221b50a62184967573f2c5d6b312ea7e49
Narcélio Rodrigues <rodriguesnarcelio5@gmail.com>
2025-04-03 13:55:22 -0300

    Aula 13 – Repetições

---
+++ JavaScript/CursoJS/Aula 13 - Repetições/15.js
@@ -0,0 +1,25 @@
+/*
+function comerPizza() {
+    while(temfatia()) {
+        comerfatia()
+    }
+}
+*/
+
+// var c = 1
+// while (c <= 6) {
+//     console.log('Tudo bem?')
+//     c++
+// }
+
+// var c = 1
+// while (c <= 6) {
+//     console.log(`Passo ${c}`)
+//     c++
+// }
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)

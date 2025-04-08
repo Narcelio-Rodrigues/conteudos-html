862ae4e1eb34c0896be43dd79bc22274bc305019
Narcélio Rodrigues <rodriguesnarcelio5@gmail.com>
2025-04-08 09:45:16 -0300

    Aula 13 – Repetições / pratica 2

--- JavaScript/CursoJS/Aula 13 - Repetições/Praticando/Prática 2/script.js
+++ JavaScript/CursoJS/Aula 13 - Repetições/Praticando/Prática 2/script.js
@@ -9,37 +9,48 @@ do {
         case '2':
             var operacoes = window.prompt('Operações Matemáticas:\n 1 - Somar \n 2 - Subtrair \n 3 - Multiplicar \n 4 - Dividir \n')
             
+            var n1 = Number.parseFloat(window.prompt('Digite o primeiro número:'))
+            var n2 = Number.parseFloat(window.prompt('Digite o segundo número:'))
+            var resultado;
+
             if (operacoes === '1') {
-                var n1 = Number.parseFloat(window.prompt('Digite o primeiro número:'))
-                var n2 = Number.parseFloat(window.prompt('Digite o segundo número:'))
-                var soma = n1 + n2
-                window.alert(`Resultado de operação: ${soma}`)
+                resultado = n1 + n2
+                window.alert(`Resultado de operação: ${resultado}`)
+
             } else if(operacoes === '2') {
-                var n1 = Number.parseFloat(window.prompt('Digite o primeiro número:'))
-                var n2 = Number.parseFloat(window.prompt('Digite o segundo número:'))
-                var subtrair = n1 - n2
-                window.alert(`Resultado da operação: ${subtrair}`)
+                resultado = n1 - n2
+                window.alert(`Resultado da operação: ${resultado}`)
+
             } else if (operacoes === '3') {
-                var n1 = Number.parseFloat(window.prompt('Digite o primeiro número:'))
-                var n2 = Number.parseFloat(window.prompt('Digite o segundo número:'))
-                var multiplicar = n1 * n2
-                window.alert(`Resultado da operação: ${multiplicar}`)
+                resultado = n1 * n2
+                window.alert(`Resultado da operação: ${resultado}`)
+
             } else if(operacoes === '4'){
-                var n1 = Number.parseFloat(window.prompt('Digite o primeiro número:'))
-                var n2 = Number.parseFloat(window.prompt('Digite o segundo número:'))
-                var dividir = n1 / n2
-                window.alert(`Resultado da operação: ${dividir}`)
+                resultado = n1 / n2
+                window.alert(`Resultado da operação: ${resultado}`)
+
             } else {
                 window.alert('[ERRO] Opção inválida!')
             }
-            break;
+
+            var continuar = window.confirm('Deseja voltar ao menu principal?')
+            if(!continuar){
+                opcao = '3'
+                break
+            }
+            break
 
         case '3':
             window.alert('Saindo...')
-            break;
+            break
 
         default:
             window.alert('[ERRO] Opção inválida!')
-            break;
+            break
     }
+
+    if(opcao !== '3') {
+       opcao = window.prompt('Escolha uma opção: \n 1 - Digite um número aleatório \n 2 - Operações Matemáticas \n 3 - Sair') 
+    }
+    
 } while (opcao !== '3')

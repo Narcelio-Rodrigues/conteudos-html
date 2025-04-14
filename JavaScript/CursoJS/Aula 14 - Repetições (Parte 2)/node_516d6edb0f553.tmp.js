6cab487b6b1015dd0b0abd27d17d31cad636744b
Narcélio Rodrigues <rodriguesnarcelio5@gmail.com>
2025-04-13 23:58:02 -0300

    Exercício 3 - Contador

--- JavaScript/CursoJS/Aula 14 - Repetições (Parte 2)/node_516d6edb0f553.tmp.js
+++
@@ -1,3 +0,0 @@
-for (let d = 0; d <= 20; d=+2) {
-    console.log(d);
-}
--- JavaScript/CursoJS/Aula 14 - Repetições (Parte 2)/node_663563515829a.tmp.js
+++
@@ -1,4 +0,0 @@
-const palavra = 'Fone'
-for (let i = 0; i < palavra.length; 1) {
-    console.log(`Letra ${i + 1}: ${palavra[i]}`)
-}

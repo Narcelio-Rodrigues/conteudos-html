10f57b0de3d7705de0bd2a3e3b79aa8b33852c64
Narcélio Rodrigues <rodriguesnarcelio5@gmail.com>
2025-04-14 01:08:36 -0300

    Exercício 3 - Contador

--- JavaScript/CursoJS/Exercícios JavaScript/Exercício 3/script.js
+++ JavaScript/CursoJS/Exercícios JavaScript/Exercício 3/script.js
@@ -7,13 +7,21 @@ function contar() {
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = 'Contando...'
+        res.innerHTML = 'Contando:'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for (let c = i; c <= f; c += p) {
-            res.innerHTML += ${c}
+        if (i < f) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            // Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
         }
-    }
+        res.innerHTML += `\u{1F3c1}`
+    } 
 }
